6649c88b5040675e001a717da34ae6c3b9ecd1ab
Luka Jacobowitz <luka.jacobowitz@gmail.com>
2019-07-26 12:10:21 -0400

    Add geoJson codec

--- core/src/main/scala/compstak/geojson/GeometryType.scala
+++ core/src/main/scala/compstak/geojson/GeometryType.scala
@@ -4,10 +4,11 @@ import cats.implicits._
 import io.circe._
 import io.circe.syntax._
 
+sealed abstract class GeoJsonObjectType(val tag: String) extends Product with Serializable
+
 sealed abstract class GeometryType(
-  val tag: String
-) extends Product
-    with Serializable
+  override val tag: String
+) extends GeoJsonObjectType(tag)
 
 object GeometryType {
 
@@ -35,6 +36,30 @@ object GeometryType {
         t <- all
           .find(_.tag === s)
           .toRight(DecodingFailure(s"Invalid geometry type: $s", List.empty))
+      } yield t
+    }
+}
+
+object GeoJsonObjectType {
+  case object GeometryCollection extends GeoJsonObjectType("GeometryCollection")
+  case object Feature extends GeoJsonObjectType("Feature")
+  case object FeatureCollection extends GeoJsonObjectType("FeatureCollection")
+
+  val all: List[GeoJsonObjectType] = GeometryCollection :: Feature ::
+    FeatureCollection :: GeometryType.all
+
+  def tag(g: GeometryType): String = g.tag
+
+  implicit val encoderForGeoJsonObjectType: Encoder[GeoJsonObjectType] = Encoder
+    .instance(_.tag.asJson)
+
+  implicit val decoderForGeoJsonObjectType: Decoder[GeoJsonObjectType] = Decoder
+    .instance { cursor =>
+      for {
+        s <- cursor.as[String]
+        t <- all
+          .find(_.tag === s)
+          .toRight(DecodingFailure(s"Invalid GeoJSON object type: $s", List.empty))
       } yield t
     }
 }
