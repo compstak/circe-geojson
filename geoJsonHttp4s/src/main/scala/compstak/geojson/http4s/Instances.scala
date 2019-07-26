f78a05ab6d9766dbe39a193aba5d5b47248dce44
Luka Jacobowitz <luka.jacobowitz@gmail.com>
2019-07-26 13:12:04 -0400

    Add all the instances

--- geoJsonHttp4s/src/main/scala/compstak/geojson/http4s/Instances.scala
+++ geoJsonHttp4s/src/main/scala/compstak/geojson/http4s/Instances.scala
@@ -4,14 +4,64 @@ package http4s
 import org.http4s.{EntityDecoder, EntityEncoder}
 import org.http4s.circe._
 import cats.effect.Sync
-import cats.kernel.Eq
-import io.circe.Encoder
-import io.circe.Decoder
+import cats.{Applicative, Eq}
+import io.circe.{Encoder, Decoder}
+import io.circe.Json
 
 object instances {
-  implicit def entityEncoderForGeoJsonGeometry[F[_]: Sync, N: Encoder]: EntityEncoder[F, GeoJsonGeometry[N]] =
+
+  implicit def entityEncoderForGeoJson[F[_]: Applicative, N: Encoder]: EntityEncoder[F, GeoJson[N]] =
+    jsonEncoderOf[F, GeoJson[N]]
+
+  implicit def entityDecoderForGeoJson[F[_]: Sync, N: Eq: Decoder]: EntityDecoder[F, GeoJson[N]] =
+    jsonOf[F, GeoJson[N]]
+
+  implicit def entityEncoderForGeoJsonGeometry[F[_]: Applicative, N: Encoder]: EntityEncoder[F, GeoJsonGeometry[N]] =
     jsonEncoderOf[F, GeoJsonGeometry[N]]
 
   implicit def entityDecoderForGeoJsonGeometry[F[_]: Sync, N: Eq: Decoder]: EntityDecoder[F, GeoJsonGeometry[N]] =
     jsonOf[F, GeoJsonGeometry[N]]
+
+    implicit def entityEncoderForPoint[F[_]: Applicative, N: Encoder]: EntityEncoder[F, Point[N]] =
+    jsonEncoderOf[F, Point[N]]
+
+  implicit def entityDecoderForPoint[F[_]: Sync, N: Decoder]: EntityDecoder[F, Point[N]] =
+    jsonOf[F, Point[N]]
+
+    implicit def entityEncoderForMultiPoint[F[_]: Applicative, N: Encoder]: EntityEncoder[F, MultiPoint[N]] =
+    jsonEncoderOf[F, MultiPoint[N]]
+
+  implicit def entityDecoderForMultiPoint[F[_]: Sync, N: Decoder]: EntityDecoder[F, MultiPoint[N]] =
+    jsonOf[F, MultiPoint[N]]
+
+    implicit def entityEncoderForLineString[F[_]: Applicative, N: Encoder]: EntityEncoder[F, LineString[N]] =
+    jsonEncoderOf[F, LineString[N]]
+
+  implicit def entityDecoderForLineString[F[_]: Sync, N: Decoder]: EntityDecoder[F, LineString[N]] =
+    jsonOf[F, LineString[N]]
+
+    implicit def entityEncoderForMultiLineString[F[_]: Applicative, N: Encoder]: EntityEncoder[F, MultiLineString[N]] =
+    jsonEncoderOf[F, MultiLineString[N]]
+
+  implicit def entityDecoderForMultiLineString[F[_]: Sync, N: Decoder]: EntityDecoder[F, MultiLineString[N]] =
+    jsonOf[F, MultiLineString[N]]
+
+    implicit def entityEncoderForPolygon[F[_]: Applicative, N: Encoder]: EntityEncoder[F, Polygon[N]] =
+    jsonEncoderOf[F, Polygon[N]]
+
+  implicit def entityDecoderForPolygon[F[_]: Sync, N: Decoder: Eq]: EntityDecoder[F, Polygon[N]] =
+    jsonOf[F, Polygon[N]]
+
+    implicit def entityEncoderForMultiPolygon[F[_]: Applicative, N: Encoder]: EntityEncoder[F, MultiPolygon[N]] =
+    jsonEncoderOf[F, MultiPolygon[N]]
+
+  implicit def entityDecoderForMultiPolygon[F[_]: Sync, N: Decoder: Eq]: EntityDecoder[F, MultiPolygon[N]] =
+    jsonOf[F, MultiPolygon[N]]
+
+    implicit def entityEncoderForGeometryCollection[F[_]: Applicative, N: Encoder]: EntityEncoder[F, GeometryCollection[N]] =
+    jsonEncoderOf[F, GeometryCollection[N]]
+
+  implicit def entityDecoderForGeometryCollection[F[_]: Sync, N: Decoder: Eq]: EntityDecoder[F, GeometryCollection[N]] =
+    jsonOf[F, GeometryCollection[N]]
+
 }
